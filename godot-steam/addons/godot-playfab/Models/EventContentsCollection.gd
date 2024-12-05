118dc305972e859f19c4777e0a0ace1d4dcce6de
Johannes Ebner <Structed@users.noreply.github.com>
2024-12-05 21:36:57 +0100

    Remove old godot-playfab version

--- godot-steam/addons/godot-playfab/Models/EventContentsCollection.gd
+++
@@ -1,5 +0,0 @@
-extends AbstractJsonSerializableCollection
-class_name EventContentsCollection
-
-func _init():
-	_item_type = EventContents
--- godot-steam/addons/godot-playfab/PlayFabConstants.gd
+++
@@ -1,5 +0,0 @@
-extends Object
-class_name PlayFabConstants
-
-
-const SETTING_PLAYFAB_TITLE_ID = "playfab/title_id"
